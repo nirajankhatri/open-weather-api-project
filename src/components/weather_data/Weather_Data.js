92d4701a71c97e4227b12c1a232040d012450daa
intern21 <intern21@pop-os.localdomain>
2023-02-24 17:17:24 +0545

    ui update

--- src/components/weather_data/Weather_Data.js
+++ src/components/weather_data/Weather_Data.js
@@ -1,25 +1,27 @@
 import React from "react";
-import '../../style/components/_weather_data.scss'
+import "../../style/components/_weather_data.scss";
 
 const Weather_Data = (props) => {
   return (
     <div className="weatherData">
-      <div className="weatherData__city-container">
-        <h1 className="weatherData__city-container__label">City Name</h1>
-        <p className="weatherData__weather-container__info">{props.data.name}</p>
-      </div>
-      <div className="weatherData__temp-container">
-        <h1 className="weatherData__temp-container__label">Temperature</h1>
-        <p className="weatherData__weather-container__info">{props.data.main.temp}°C</p>
-      </div>
-      <div className="weatherData__humid-container">
-        <h1 className="weatherData__humid-container__label">Humidity</h1>
-        <p className="weatherData__weather-container__info">{props.data.main.humidity}%</p>
-      </div>
-      <div className="weatherData__weather-container">
-        <h1 className="weatherData__weather-container__label">Weather</h1>
-        <p className="weatherData__weather-container__info">{props.data.weather[0].description}</p>
-      </div>
+      <table className="weatherData__table">
+        <tr className="weatherData__table__row">
+          <td className="weatherData__table__row__label">City Name</td>
+          <td className="weatherData__table__row__info">{props.data.name}</td>
+        </tr>
+        <tr className="weatherData__table__row">
+          <td className="weatherData__table__row__label">Temperature</td>
+          <td className="weatherData__table__row__info">{props.data.main.temp}°C</td>
+        </tr>
+        <tr className="weatherData__table__row">
+          <td className="weatherData__table__row__label">Humidity</td>
+          <td className="weatherData__table__row__info">{props.data.main.humidity}%</td>
+        </tr>
+        <tr className="weatherData__table__row">
+          <td className="weatherData__table__row__label">Weather</td>
+          <td className="weatherData__table__row__info">{props.data.weather[0].description}</td>
+        </tr>
+      </table>
     </div>
   );
 };
